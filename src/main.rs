5356dbba2ec11d0f98165a225fa963a3e8ccb59b
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-07-20 01:55:42 -0400

    moved time limit to 50 mins and foramted and clippied it

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-use chrono;
-use std::env::consts::OS;
-use std::io::{self, Write};
-use std::process::{Command, exit};
-use std::thread;
-use std::time::{Duration, Instant};
-use std::env;
-use home;
 use crossterm::{
     event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
     terminal,
 };
+
+use std::env;
+use std::env::consts::OS;
+use std::io::{self, Write};
+use std::process::{exit, Command};
+use std::thread;
+use std::time::{Duration, Instant};
 // Todo make window pop up on top of all other windows if help is requested and pause discord unrtill help is closed
 // Todo make the interval for checking for Discord more often
 fn main() {
     let mut day = chrono::Local::today();
-    let mut time_left = 120;
-    let mut help_time= 5; // this defines time for help peroid
+    let mut time_left = 50;
+    let mut help_time = 5; // this defines time for help peroid
     if env::args().len() > 1 {
         help_time = env::args().nth(1).unwrap().parse::<u64>().unwrap();
     }
@@ -25,9 +24,37 @@ fn main() {
         if day != chrono::Local::today() {
             println!("New day!");
             day = chrono::Local::today();
-            time_left = 120;
+            time_left = 50;
         }
         println!("{}", time_left);
+        println!("type 'h' for help");
+        let help = read_key(Duration::from_secs(60));
+        match help {
+            // Todo figureout how to start discord on mac and linux
+            Some(KeyEvent {
+                code: KeyCode::Char('h'),
+                modifiers: KeyModifiers::NONE,
+            }) => {
+                println!("Help requested!");
+                let dir = home::home_dir().unwrap();
+                let full = format!("{}\\Desktop\\Discord.lnk", dir.to_str().unwrap());
+                Command::new("powershell")
+                    .arg(full)
+                    .spawn()
+                    .expect("failed to open Discord");
+                thread::sleep(Duration::from_secs(help_time * 60));
+            }
+            Some(KeyEvent {
+                code: KeyCode::Char('c'),
+                modifiers: KeyModifiers::CONTROL,
+            }) => {
+                println!();
+                exit(1);
+            }
+            _ => {
+                println!("Help not requested!");
+            }
+        }
         let ps: String = match OS {
             "windows" => {
                 let output = Command::new("powershell")
@@ -55,8 +82,8 @@ fn main() {
         // check if there is time left
         if time_left == 0 && !ps.is_empty() {
             // loop {
-                // print!("Discord is running!\nIf you are using it for help, type \"help\" to continue using it: ");
-                io::stdout().flush().unwrap();
+            // print!("Discord is running!\nIf you are using it for help, type \"help\" to continue using it: ");
+            io::stdout().flush().unwrap();
 
             println!("Time's up!");
             match OS {
@@ -71,39 +98,12 @@ fn main() {
                     .spawn()
                     .expect("failed to close Discord"),
             };
-            print!("Discord is running!\nIf you are using it for help, type \"h\" to continue using it: ");
+            // print!("Discord is running!\nIf you are using it for help, type \"h\" to continue using it: ");
             io::stdout().flush().unwrap();
-            let help = read_key(Duration::from_secs(60));
-            match help {
-                // Todo figureout how to start discord on mac and linux
-                Some(KeyEvent {
-                    code: KeyCode::Char('h'),
-                    modifiers: KeyModifiers::NONE,
-                }) => {
-                    println!("Help requested!");
-                    let dir = home::home_dir().unwrap();
-                    let full = format!("{}\\Desktop\\Discord.lnk", dir.to_str().unwrap());
-                    Command::new("powershell").arg(full).spawn().expect("failed to open Discord");
-                    thread::sleep(Duration::from_secs(help_time*60));
-
-            },
-                Some(KeyEvent {
-                    code: KeyCode::Char('c'),
-                    modifiers: KeyModifiers::CONTROL,
-                }) => {
-                    println!("");
-                    exit(1);
-                },
-                _ => {
-                    println!("Help not requested!");
-                }
-
-        }
-        }
-         else if !ps.is_empty() {
+        } else if !ps.is_empty() {
             time_left -= 2;
         }
-        
+
         // wait for 2 minutes
         thread::sleep(std::time::Duration::from_secs(120));
     }
@@ -116,7 +116,7 @@ fn read_key(timeout: Duration) -> Option<KeyEvent> {
             terminal::disable_raw_mode().unwrap();
         }
     }
-    
+
     terminal::enable_raw_mode().unwrap();
     let _guard = RawModeGuard;
     let start = Instant::now();
@@ -127,5 +127,5 @@ fn read_key(timeout: Duration) -> Option<KeyEvent> {
         }
         offset = start.elapsed();
     }
-    return None;
+    None
 }
